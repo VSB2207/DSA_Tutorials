234ee8f06cd2de8b3bc7b3915910ce2ae6511bd7
VSB2207 <singh.vaibhav0798@gmail.com>
2020-08-09 02:23:33 +0530

    datatype

--- src/DsaTutorial/Datatypes.java
+++ src/DsaTutorial/Datatypes.java
@@ -4,7 +4,18 @@ public class Datatypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		float rateOfInterest=2.3f;
+		double rate = 3.1416;
+		System.out.println(rate);
+		boolean isThisSeriesCool=false;
+		 char myCharacter='@';
+			System.out.println(a);
+			System.out.println(isThisSeriesCool);
+			System.out.println(myCharacter);
+			System.out.println(rateOfInterest);
 
+			
 	}
 
 }
